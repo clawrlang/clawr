4ef87175f37245427babb1cc7976b327594443ed
Johan Segerfeldt <johan.segerfeldt@me.com>
2026-06-19 13:15:01 +0200

    Prepare test harness for running e2e tests

---
+++ src/rwrc/index.ts
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+import fs from 'fs'
+import path from 'path'
+import { Command } from 'commander'
+import Bun from 'bun'
+
+const exeDir = path.dirname(process.execPath)
+const program = new Command()
+
+program.name('rwrc').description('Clawr language compiler').version('0.1.0')
+
+program
+    .command('build <file>')
+    .option('-o, --outdir <dir>', 'Output directory', 'dist')
+    .description('Compile a Clawr source file')
+    .action(async (file: string, options: { outdir: string }) => {
+        const resolvedOutDir = path.resolve(options.outdir)
+        const cFilePath = `${resolvedOutDir}/${path.basename(file).replace(/.clawr$/, '.c')}`
+        const exePath = `${resolvedOutDir}/${path.basename(file).replace(/.clawr$/, '')}`
+
+        const cCode = `#include <stdio.h>
+            int main() {
+                printf("%d\\n", 1);
+                return 0;
+            }`
+        try {
+            if (!fs.existsSync(resolvedOutDir))
+                fs.mkdirSync(resolvedOutDir, { recursive: true })
+            fs.writeFileSync(cFilePath, cCode)
+
+            const proc = Bun.spawn(
+                [
+                    'clang',
+                    '-I',
+                    path.join(exeDir, 'include'),
+                    cFilePath,
+                    path.join(exeDir, 'libClawr.A.dylib'),
+                    '-o',
+                    exePath,
+                ],
+                {
+                    stdout: 'inherit',
+                    stderr: 'inherit',
+                },
+            )
+            await proc.exited
+        } catch (err) {
+            console.error(err instanceof Error ? err.message : err)
+            // For debugging:
+            // console.error('error:', err)
+            process.exit(1)
+        }
+    })
+
+program.parse(process.argv)

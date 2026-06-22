404d68e802ac57ac89171138a768e952be4b927f
Johan Segerfeldt <johan.segerfeldt@me.com>
2026-06-22 20:41:53 +0200

    Remove redundant TestErrorReporter

--- tests/unit/parser/expression-parser.spec.ts
+++ tests/unit/parser/expression-parser.spec.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it } from 'bun:test'
-import { ErrorReporter, SourceCodeSpan } from '../../../src/diagnostics'
 import { TokenStream } from '../../../src/lexer'
 import { ExpressionParser } from '../../../src/parser'
 import { Expression } from '../../../src/model'
+import { TestErrorReporter } from '../../util'
 
 describe('Expression Parser', () => {
     describe('truthvalue literals', () => {
@@ -27,18 +27,9 @@ describe('Expression Parser', () => {
 })
 
 function parseLiteral(input: string): Expression {
-    const tokenStream = TokenStream.read(input, new TestErrorReporter())
+    const tokenStream = TokenStream.read(
+        input,
+        new TestErrorReporter('test.clawr'),
+    )
     return ExpressionParser.create(tokenStream).parse()
-}
-
-class TestErrorReporter implements ErrorReporter {
-    reportFatalError(message: string, location: SourceCodeSpan): never {
-        throw new Error('Method not implemented.')
-    }
-    reportWarning(message: string, location: SourceCodeSpan): void {
-        throw new Error('Method not implemented.')
-    }
-    reportError(message: string, location: SourceCodeSpan): void {
-        throw new Error('Method not implemented.')
-    }
 }

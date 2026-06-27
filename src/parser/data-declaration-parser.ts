11720bb8d5eb809170b35a8244ce01a53dccdacc
Johan Segerfeldt <johan.segerfeldt@me.com>
2026-06-27 08:01:49 +0200

    Parse explicit field semantics

--- src/parser/data-declaration-parser.ts
+++ src/parser/data-declaration-parser.ts
@@ -1,5 +1,9 @@
 import { TokenStream } from '../lexer'
 import { DataDeclaration } from '../model/data-declaration'
+import {
+    VARIABLE_KINDS,
+    VariableSemantics,
+} from '../model/variable-declaration'
 
 export class DataDeclarationParser {
     private constructor() {}
@@ -16,17 +20,26 @@ export class DataDeclarationParser {
         const fields: {
             name: string
             type: string
-            semantics: 'const' | 'mut' | 'ref' | 'mutref'
+            semantics: VariableSemantics
         }[] = []
         while (!stream.isNext('PUNCTUATION', '}')) {
+            let semantics = this.parseFieldSemantics(stream)
             const fieldNameToken = stream.expect('IDENTIFIER')
             const fieldName = fieldNameToken.identifier
             stream.expect('PUNCTUATION', ':')
             const fieldTypeToken = stream.expect('IDENTIFIER')
             const fieldType = fieldTypeToken.identifier
-            fields.push({ name: fieldName, type: fieldType, semantics: 'mut' }) // Default semantics to 'mut' for now
+            fields.push({ name: fieldName, type: fieldType, semantics })
         }
         stream.expect('PUNCTUATION', '}')
         return DataDeclaration.create({ name, fields })
+    }
+
+    private parseFieldSemantics(stream: TokenStream) {
+        if (stream.isNext('KEYWORD', ...VARIABLE_KINDS)) {
+            const semanticsToken = stream.expect('KEYWORD', ...VARIABLE_KINDS)
+            return semanticsToken.keyword as VariableSemantics
+        }
+        return 'mut'
     }
 }
--- tests/unit/parser/data-declaration-parser.spec.ts
+++ tests/unit/parser/data-declaration-parser.spec.ts
@@ -4,7 +4,7 @@ import { TestErrorReporter } from '../../util'
 import { DataDeclarationParser } from '../../../src/parser/data-declaration-parser'
 
 describe('DataDeclarationParser', () => {
-    it('parses a data declaration', () => {
+    it('parses a data declaration with default-mutability', () => {
         const code = `
             data MyData {
                 field1: integer
@@ -17,8 +17,27 @@ describe('DataDeclarationParser', () => {
         expect(result).toMatchObject({
             name: 'MyData',
             fields: [
-                { name: 'field1', type: 'integer' },
-                { name: 'field2', type: 'truthvalue' },
+                { name: 'field1', type: 'integer', semantics: 'mut' },
+                { name: 'field2', type: 'truthvalue', semantics: 'mut' },
+            ],
+        })
+    })
+
+    it('parses a data declaration with mixed semantics', () => {
+        const code = `
+            data MyData {
+                ref field1: integer
+                const field2: truthvalue
+            }`
+        const errorReporter = new TestErrorReporter('test.clawr')
+        const tokenStream = TokenStream.read(code, errorReporter)
+        const parser = DataDeclarationParser.create()
+        const result = parser.parse(tokenStream)
+        expect(result).toMatchObject({
+            name: 'MyData',
+            fields: [
+                { name: 'field1', type: 'integer', semantics: 'ref' },
+                { name: 'field2', type: 'truthvalue', semantics: 'const' },
             ],
         })
     })

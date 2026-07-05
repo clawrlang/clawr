c20e991204f4df30c653f8ec1c8ce04157930c17
Johan Segerfeldt <johan.segerfeldt@me.com>
2026-07-05 23:09:50 +0200

    Inline all types in union declarations

--- src/cir/index.ts
+++ src/cir/index.ts
@@ -4,106 +4,86 @@ export type ClawrModule = {
     declarations?: Declaration[]
 }
 
-export type Declaration = VariableDeclaration | DataDeclaration
-
-type VariableDeclaration = {
-    kind: 'VARIABLE_DECL'
-    name: string
-    type: string
-    initialValue: Expression
-}
-
-type DataDeclaration = {
-    kind: 'DATA_DECL'
-    name: string
-    fields: {
-        name: string
-        type: string
-    }[]
-}
+export type Declaration =
+    | {
+          kind: 'VARIABLE_DECL'
+          name: string
+          type: string
+          initialValue: Expression
+      }
+    | {
+          kind: 'DATA_DECL'
+          name: string
+          fields: {
+              name: string
+              type: string
+          }[]
+      }
 
 export type Statement =
-    | EnsureUnique
-    | Release
-    | CallFunc
-    | VariableDeclaration
-    | Assignment
-
-type EnsureUnique = {
-    kind: 'ENSURE_UNIQUE'
-    object: Expression
-}
-
-type CallFunc = {
-    kind: 'CALL_FUNC'
-    signature: {
-        baseName: string
-        parameters: {
-            label?: string
-            type: string
-        }[]
-    }
-    arguments: Expression[]
-}
-
-type Release = {
-    kind: 'RELEASE'
-    object: VariableReference | FieldReference
-}
-
-type Assignment = {
-    kind: 'ASSIGN'
-    target: VariableReference | FieldReference
-    value: Expression
-}
+    | {
+          kind: 'ENSURE_UNIQUE'
+          object: Expression
+      }
+    | {
+          kind: 'RELEASE'
+          object: Extract<Expression, { kind: 'VARIABLE_REF' | 'FIELD_REF' }>
+      }
+    | {
+          kind: 'CALL_FUNC'
+          signature: {
+              baseName: string
+              parameters: {
+                  label?: string
+                  type: string
+              }[]
+          }
+          arguments: Expression[]
+      }
+    | Extract<Declaration, { kind: 'VARIABLE_DECL' }>
+    | {
+          kind: 'ASSIGN'
+          target: Extract<Expression, { kind: 'VARIABLE_REF' | 'FIELD_REF' }>
+          value: Expression
+      }
 
 export type Expression =
-    | StringLiteral
-    | IntegerLiteral
-    | TruthValueLiteral
-    | Allocation
-    | Retain
-    | VariableReference
-    | FieldReference
-    | CallFunc
-
-type StringLiteral = {
-    kind: 'STRING_LITERAL'
-    value: string
-}
-
-type IntegerLiteral = {
-    kind: 'INTEGER_LITERAL'
-    value: string
-}
-
-type TruthValueLiteral = {
-    kind: 'TRUTHVALUE_LITERAL'
-    value: 'false' | 'ambiguous' | 'true'
-}
-
-type Allocation = {
-    kind: 'ALLOCATE'
-    type: string
-    semantics: 'REF' | 'COW'
-    fields: {
-        name: string
-        value: Expression
-    }[]
-}
-
-type Retain = {
-    kind: 'RETAIN'
-    object: VariableReference | FieldReference
-}
-
-type VariableReference = {
-    kind: 'VARIABLE_REF'
-    name: string
-}
-
-type FieldReference = {
-    kind: 'FIELD_REF'
-    object: Expression
-    field: string
-}
+    | {
+          kind: 'STRING_LITERAL'
+          value: string
+      }
+    | {
+          kind: 'INTEGER_LITERAL'
+          value: string
+      }
+    | {
+          kind: 'TRUTHVALUE_LITERAL'
+          value: 'false' | 'ambiguous' | 'true'
+      }
+    | {
+          kind: 'ALLOCATE'
+          type: string
+          semantics: 'REF' | 'COW'
+          fields: {
+              name: string
+              value: Expression
+          }[]
+      }
+    | {
+          kind: 'RETAIN'
+          object: Extract<Expression, { kind: 'VARIABLE_REF' | 'FIELD_REF' }>
+      }
+    | {
+          kind: 'VARIABLE_REF'
+          name: string
+      }
+    | {
+          kind: 'VARIABLE_REF'
+          name: string
+      }
+    | {
+          kind: 'FIELD_REF'
+          object: Expression
+          field: string
+      }
+    | Extract<Statement, { kind: 'CALL_FUNC' }>

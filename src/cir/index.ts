ec6a58394eaa2d259fc38b73034a8036233c68e3
Johan Segerfeldt <johan.segerfeldt@me.com>
2026-07-13 08:36:15 +0200

    Separate into named types
    Also remove unused `fields` property and add `real` type

--- src/cir/index.ts
+++ src/cir/index.ts
@@ -4,130 +4,166 @@ export type ClawrModule = {
     declarations?: Declaration[]
 }
 
+type VariableDeclaration = {
+    kind: 'VARIABLE_DECL'
+    name: string
+    valueSet: ValueSet
+    initialValue: Expression
+}
+
+type FunctionDeclaration = {
+    kind: 'FUNCTION_DECL'
+    baseName: string
+    parameters: {
+        label?: string
+        varName: string
+        valueSet: ValueSet
+    }[]
+    body: Statement[]
+    returnValueSet?: ValueSet
+}
+
+type DataDeclaration = {
+    kind: 'DATA_DECL'
+    name: string
+    fields: {
+        name: string
+        valueSet: ValueSet
+    }[]
+}
+
 export type Declaration =
-    | {
-          kind: 'VARIABLE_DECL'
-          name: string
-          valueSet: ValueSet
-          initialValue: Expression
-      }
-    | {
-          kind: 'FUNCTION_DECL'
-          baseName: string
-          parameters: {
-              label?: string
-              varName: string
-              valueSet: ValueSet
-          }[]
-          body: Statement[]
-          returnValueSet?: ValueSet
-      }
-    | {
-          kind: 'DATA_DECL'
-          name: string
-          fields: {
-              name: string
-              valueSet: ValueSet
-          }[]
-      }
+    VariableDeclaration | FunctionDeclaration | DataDeclaration
+
+type EnsureUnique = {
+    kind: 'ENSURE_UNIQUE'
+    object: Storage
+}
+
+type Release = {
+    kind: 'RELEASE'
+    object: Storage
+}
+
+type Exec = {
+    kind: 'EXEC'
+    name: {
+        baseName: string
+        labels: string[]
+    }
+    arguments: Expression[]
+}
+
+type Return = {
+    kind: 'RETURN'
+    value?: Expression
+}
+
+type Assign = {
+    kind: 'ASSIGN'
+    target: Storage
+    value: Expression
+}
 
 export type Statement =
-    | {
-          kind: 'ENSURE_UNIQUE'
-          object: Extract<Expression, { kind: 'VARIABLE_REF' | 'FIELD_REF' }>
-      }
-    | {
-          kind: 'RELEASE'
-          object: Extract<Expression, { kind: 'VARIABLE_REF' | 'FIELD_REF' }>
-      }
-    | {
-          kind: 'EXEC'
-          name: {
-              baseName: string
-              labels: string[]
-          }
-          arguments: Expression[]
-      }
-    | {
-          kind: 'RETURN'
-          value?: Expression
-      }
-    | Extract<Declaration, { kind: 'VARIABLE_DECL' }>
-    | {
-          kind: 'ASSIGN'
-          target: Extract<Expression, { kind: 'VARIABLE_REF' | 'FIELD_REF' }>
-          value: Expression
-      }
+    EnsureUnique | Release | Exec | Return | VariableDeclaration | Assign
+
+type Storage = VariableReference | FieldReference
+
+type StringLiteral = {
+    kind: 'STRING_LITERAL'
+    value: string
+    valueSet: StringValueSet
+}
+
+type IntegerLiteral = {
+    kind: 'INTEGER_LITERAL'
+    value: string
+    valueSet: IntegerValueSet
+}
+
+type TruthLiteral = {
+    kind: 'TRUTHVALUE_LITERAL'
+    value: 'false' | 'ambiguous' | 'true'
+    valueSet: TruthValueSet
+}
+
+type MemoryAllocation = {
+    kind: 'ALLOCATE'
+    valueSet: RcTypeValueSet
+    fields: {
+        name: string
+        value: Expression
+    }[]
+}
+
+type MemoryRetention = {
+    kind: 'RETAIN'
+    object: Storage
+    valueSet: RcTypeValueSet
+}
+
+type VariableReference = {
+    kind: 'VARIABLE_REF'
+    name: string
+    valueSet: ValueSet
+}
+
+type FieldReference = {
+    kind: 'FIELD_REF'
+    object: Expression
+    field: string
+    valueSet: ValueSet
+}
+
+type QueryFunctionCall = {
+    kind: 'QUERY'
+    name: {
+        baseName: string
+        labels: string[]
+    }
+    arguments: Expression[]
+    valueSet: ValueSet
+}
 
 export type Expression =
-    | {
-          kind: 'STRING_LITERAL'
-          value: string
-          valueSet: Extract<ValueSet, { type: 'string' }>
-      }
-    | {
-          kind: 'INTEGER_LITERAL'
-          value: string
-          valueSet: Extract<ValueSet, { type: 'integer' }>
-      }
-    | {
-          kind: 'TRUTHVALUE_LITERAL'
-          value: 'false' | 'ambiguous' | 'true'
-          valueSet: Extract<ValueSet, { type: 'truthvalue' }>
-      }
-    | {
-          kind: 'ALLOCATE'
-          valueSet: Extract<ValueSet, { type: 'rc-type' }>
-          fields: {
-              name: string
-              value: Expression
-          }[]
-      }
-    | {
-          kind: 'RETAIN'
-          object: Extract<Expression, { kind: 'VARIABLE_REF' | 'FIELD_REF' }>
-          valueSet: Extract<ValueSet, { type: 'rc-type' }>
-      }
-    | {
-          kind: 'VARIABLE_REF'
-          name: string
-          valueSet: ValueSet
-      }
-    | {
-          kind: 'FIELD_REF'
-          object: Expression
-          field: string
-          valueSet: ValueSet
-      }
-    | {
-          kind: 'QUERY'
-          name: {
-              baseName: string
-              labels: string[]
-          }
-          arguments: Expression[]
-          valueSet: ValueSet
-      }
+    | StringLiteral
+    | IntegerLiteral
+    | TruthLiteral
+    | MemoryAllocation
+    | MemoryRetention
+    | VariableReference
+    | FieldReference
+    | QueryFunctionCall
+
+type IntegerValueSet = {
+    type: 'integer'
+    min?: string
+    max?: string
+}
+
+type RealValueSet = {
+    type: 'real'
+    min?: string // numeric, can be arbitrarity big
+    max?: string // numeric, can be arbitrarity big
+}
+
+type TruthValueSet = {
+    type: 'truthvalue'
+    values: ('false' | 'ambiguous' | 'true')[]
+}
+
+type StringValueSet = { type: 'string' }
+
+type RcTypeValueSet = {
+    type: 'rc-type'
+    typeName: string
+    semantics: 'REF' | 'COW' | 'UNIQUE'
+}
 
 export type ValueSet =
-    | {
-          type: 'integer'
-          min?: string
-          max?: string
-      }
-    | {
-          type: 'truthvalue'
-          values: ('false' | 'ambiguous' | 'true')[]
-      }
-    | {
-          type: 'string'
-      }
-    | {
-          type: 'rc-type'
-          typeName: string
-          semantics: 'REF' | 'COW' | 'UNIQUE'
-          fields?: {
-              name: string
-              valueSet: ValueSet
-          }[]
-      }
+    | IntegerValueSet
+    | RealValueSet
+    | TruthValueSet
+    | StringValueSet
+    | RcTypeValueSet
